17747cae1dc0454b49c2ed20e4f33d64432f2f0e
Abhishek23232 <127471560+Abhishek23232@users.noreply.github.com>
2023-03-15 19:15:49 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,13 @@
+class Area{
+public static void main(String args[])
+{
+	System.out.println("The values are:");
+	int l=3;
+	int b=4;
+	System.out.println(l+ " " +b);
+	int Area =l*b;
+	int Perimeter=2*l+b;
+	System.out.println("Area of Rectangle is:"+Area);
+	System.out.println("Perimeter of Rectangle is:"+Perimeter);
+}
+}
